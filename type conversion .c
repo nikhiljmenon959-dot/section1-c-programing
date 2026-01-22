f1ae59ed018619df209bf74eb3f8833b41d649ed
nikhiljmenon959-dot <nikhiljmenon959@gmail.com>
2026-01-22 11:19:49 +0530

    Create type conversion .c

---
+++ type conversion .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 5, b = 2;
+    float result = (float)a / b;
+    printf("Result = %.2f", result);
+    return 0;
+}
+
+Result = 2.50
+
+=== Code Execution Successful ===
