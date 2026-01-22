65d2c18e6fbab6398df6e15cb69015000c071823
nikhiljmenon959-dot <nikhiljmenon959@gmail.com>
2026-01-22 10:49:07 +0530

    Create INT FLOAT CHAR.c

---
+++ INT FLOAT CHAR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    const int PI = 3;
+    printf("%d\tnijjk\t%.2f %c\tghj\t%d", a, b, c, PI);
+    return 0;
+}
+
+ Output
+10	nijjk	5.50 A	ghj	3
