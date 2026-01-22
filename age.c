cf8c711041dfb092b3d33bf9966589a230ca3af5
nikhiljmenon959-dot <nikhiljmenon959@gmail.com>
2026-01-22 10:37:32 +0530

    Add age input and output functionality in age.c

---
+++ age.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Your age is %d", age);
+    return 0;
+}
+Out put
+Enter your age: 18
+Your age is 18
+
+=== Code Execution Successful ===
